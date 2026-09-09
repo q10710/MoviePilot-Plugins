98a9290e58c04046b6d4d3bdf69879d3658e69c2
q10710 <q1071091473@gmail.com>
2026-09-09 22:14:59 +0800

    Bump to icon version: BestVersionGuard

--- plugins.v2/bestversionguard/__init__.py
+++ plugins.v2/bestversionguard/__init__.py
@@ -33,8 +33,8 @@ class BestVersionGuard(_PluginBase):
 
     plugin_name = "洗版守护"
     plugin_desc = "定时检查电视剧订阅：未完结的误标洗版自动取消，恢复普通订阅继续追更。"
-    plugin_icon = "bestversionguard.png"
-    plugin_version = "2.4.5"
+    plugin_icon = "https://raw.githubusercontent.com/q10710/MoviePilot-Plugins/main/icons/bestversionguard.png"
+    plugin_version = "2.4.6"
     plugin_label = "订阅"
     plugin_author = "local"
     plugin_config_prefix = "bestversionguard_"
