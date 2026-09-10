51aed1fc5cc8cbf0ed734d9bb9595433822c5566
q10710 <q1071091473@gmail.com>
2026-09-10 10:51:26 +0800

    Publish SeedSourceGuard v1.1.6: fix Transmission red torrent detection

--- plugins.v2/seedsourceguard/__init__.py
+++ plugins.v2/seedsourceguard/__init__.py
@@ -32,7 +32,7 @@ class SeedSourceGuard(_PluginBase):
     plugin_desc = ("检测本地源文件是否有下载器在做种、下载器是否存在文件丢失的无效做种或"
                    "tracker 全部失败的做种任务；连续N天异常可通知或按策略处置，杜绝无效做种与孤儿文件。")
     plugin_icon = "https://raw.githubusercontent.com/q10710/MoviePilot-Plugins/main/icons/seedsourceguard.png"
-    plugin_version = "1.1.5"
+    plugin_version = "1.1.6"
     plugin_label = "下载管理"
     plugin_author = "local"
     plugin_config_prefix = "seedsourceguard_"
