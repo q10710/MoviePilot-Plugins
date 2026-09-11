90ad2769c8314c65d1bf3df9eb62b748c107084a
q10710 <q1071091473@gmail.com>
2026-09-12 02:43:46 +0800

    Publish TrafficAssistantQ (author -> Q)

--- plugins.v2/trafficassistantq/__init__.py
+++ plugins.v2/trafficassistantq/__init__.py
@@ -46,9 +46,9 @@ class TrafficAssistantQ(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/q10710/MoviePilot-Plugins/main/icons/trafficassistantq.png"
     # 插件版本
-    plugin_version = "2.0.2"
+    plugin_version = "2.0.3"
     # 插件作者
-    plugin_author = "q1071091473"
+    plugin_author = "Q"
     # 作者主页
     author_url = "https://github.com/q10710"
     # 插件配置项ID前缀
