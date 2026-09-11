3770aa0e866c262b354d43fb0ee3f27c03783d32
q10710 <q1071091473@gmail.com>
2026-09-12 02:30:28 +0800

    Publish SeedSourceGuard (rename + author unify)

--- plugins.v2/seedsourceguard/__init__.py
+++ plugins.v2/seedsourceguard/__init__.py
@@ -28,13 +28,13 @@ from app.schemas.types import EventType, NotificationType
 class SeedSourceGuard(_PluginBase):
     """做种守卫插件。"""
 
-    plugin_name = "做种守卫"
+    plugin_name = "做种守卫Q自用版"
     plugin_desc = ("检测本地源文件是否有下载器在做种、下载器是否存在文件丢失的无效做种或"
                    "tracker 全部失败的做种任务；连续N天异常可通知或按策略处置，杜绝无效做种与孤儿文件。")
     plugin_icon = "https://raw.githubusercontent.com/q10710/MoviePilot-Plugins/main/icons/seedsourceguard.png"
-    plugin_version = "1.1.7"
+    plugin_version = "1.1.8"
     plugin_label = "下载管理"
-    plugin_author = "local"
+    plugin_author = "q1071091473"
     plugin_config_prefix = "seedsourceguard_"
     plugin_order = 66
     auth_level = 1
