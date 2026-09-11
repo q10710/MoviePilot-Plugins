9d13e80c39e1155f7b30901805957dcd31460573
q10710 <q1071091473@gmail.com>
2026-09-12 02:43:29 +0800

    Publish LinkChecker (author -> Q)

--- plugins.v2/linkchecker/__init__.py
+++ plugins.v2/linkchecker/__init__.py
@@ -32,9 +32,9 @@ class LinkChecker(_PluginBase):
     plugin_name = "硬链接检查Q自用版"
     plugin_desc = "扫描下载目录和媒体库目录中的孤立硬链接文件，连续3天孤立自动删除；并可清理只剩元数据、没有视频的空壳季目录/剧目录。"
     plugin_icon = "https://raw.githubusercontent.com/q10710/MoviePilot-Plugins/main/icons/linkchecker.png"
-    plugin_version = "3.3.2"
+    plugin_version = "3.3.3"
     plugin_label = "文件管理"
-    plugin_author = "q1071091473"
+    plugin_author = "Q"
     plugin_config_prefix = "linkchecker_"
     plugin_order = 50
     auth_level = 1
